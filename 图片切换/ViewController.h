e64f0515dca02296ae3836c71d93ac0a2b9df837
wzpziyi1 <1204473090@qq.com>
2015-12-17 14:42:33 +0800

    Initial Commit

---
+++ 图片切换/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片切换
+//
+//  Created by 王志盼 on 15/12/17.
+//  Copyright © 2015年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 图片切换/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  图片切换
+//
+//  Created by 王志盼 on 15/12/17.
+//  Copyright © 2015年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
